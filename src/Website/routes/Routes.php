d1ffdf6fa0473700f4f77c2bd7bf3f36092e4465
Alessandro <alessandro.antonini6@studio.unibo.it>
2023-06-22 17:55:28 +0200

    add user route

--- src/Website/routes/Routes.php
+++ src/Website/routes/Routes.php
@@ -1,6 +1,11 @@
 <?php
 
+use CaveResistance\Echo\Server\Http\Messages\ResponseBuilder;
 use CaveResistance\Echo\Server\Server;
 use CaveResistance\Echo\Website\App\Http\Controllers\HomeController;
 
 Server::createRoute()->accept('GET', ['/', '/home'])->setHandler(HomeController::class)->add();
+
+Server::createRoute()->accept('GET', '/user/{username}')->setHandler(function($username) {
+    return (new ResponseBuilder())->setContent("Hi $username")->build();
+})->add();
