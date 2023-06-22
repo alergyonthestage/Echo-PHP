80b1faa29fa994d0b08f9937773bc3ae78467612
Emanuele Bertolero <bertoleroemanuele@gmail.com>
2023-06-22 22:36:23 +0200

    Added "Feed" route

--- src/Website/routes/Routes.php
+++ src/Website/routes/Routes.php
@@ -3,7 +3,10 @@
 use CaveResistance\Echo\Server\Server;
 use CaveResistance\Echo\Website\App\Http\Controllers\HomeController;
 use CaveResistance\Echo\Website\App\Http\Controllers\UserController;
+use CaveResistance\Echo\Website\App\Http\Controllers\FeedController;
 
-Server::createRoute()->accept('GET', ['/', '/home'])->setHandler(HomeController::class)->add();
+Server::createRoute()->accept('GET', '/home')->setHandler(HomeController::class)->add();
 
 Server::createRoute()->accept('GET', '/user/{username}')->setHandler(UserController::class)->add();
+
+Server::createRoute()->accept('GET', ['/', '/feed'])->setHandler(FeedController::class)->add();
