8a7b137e453ff28752de2089fcfbb583c64ac1cd
Emanuele Bertolero <bertoleroemanuele@gmail.com>
2023-06-23 12:19:49 +0200

    Added description to the post and made a draft of a feed with multiple posts

--- src/Website/views/feed.php
+++ src/Website/views/feed.php
@@ -4,42 +4,133 @@
 </div>
 <div class="post-container">
     <div class="post">
-        <div class="covert-art">
-            <div class="cover-square">
-                <img class="cover-disc" src="/public/img/cover/1.jpg" alt="Cover-Art"/>
-                <button class="play-button"><i class="fas fa-play"></i></button>
-            </div>
-        </div>
-        <div class="post-info">
-            <div class="post-author-infos">
-                <img class="profile-picture" src="/public/img/profiles/alergyonthestage.png" alt="Profile picture">
-                <div class="author-infos">
-                    <div class="author-name">
-                        <p>alergyonthestage</p>
-                    </div>
-                    <div class="time-info">
-                        <p>5 ore fa</p>
-                    </div>
-                </div>
-                 
-            </div>
-            <div class="post-song-infos">
-                <div class="post-song-title">
-                    <p>Into You - Ariana</p>
+        <div class="post-header">
+            <div class="covert-art">
+                <div class="cover-square">
+                    <img class="cover-disc" src="/public/img/cover/1.jpg" alt="Cover-Art"/>
+                    <button class="play-button"><i class="fas fa-play"></i></button>
                 </div>
             </div>
-            <div class="post-interactive-buttons">
-            <button class="post-button active">
-                <i class="fas fa-heart"></i>
-            </button>
-            <button class="post-button">
-                <i class="fas fa-comment"></i>
-            </button>
-            <button class="post-button">
-                <i class="fas fa-user"></i>
-            </button>
+            <div class="post-info">
+                <div class="post-author-infos">
+                    <img class="profile-picture" src="/public/img/profiles/alergyonthestage.png" alt="Profile picture">
+                    <div class="author-infos">
+                        <div class="author-name">
+                            <p>alergyonthestage</p>
+                        </div>
+                        <div class="time-info">
+                            <p>5 ore fa</p>
+                        </div>
+                    </div> 
+                </div>
+                <div class="post-song-infos">
+                    <div class="post-song-title">
+                        <p>Into You - Ariana</p>
+                    </div>
+                    </div>
+                    <div class="post-interactive-buttons">
+                    <button class="post-button active">
+                        <i class="fas fa-heart"></i>
+                    </button>
+                    <button class="post-button">
+                        <i class="fas fa-comment"></i>
+                    </button>
+                    <button class="post-button">
+                        <i class="fas fa-user"></i>
+                    </button>
+                </div>
             </div>
         </div>
+        <div class="post-description">
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
+        </div>
+        <hr class="post-divider">
     </div>
-    
+    <div class="post">
+        <div class="post-header">
+            <div class="covert-art">
+                <div class="cover-square">
+                    <img class="cover-disc" src="/public/img/cover/1.jpg" alt="Cover-Art"/>
+                    <button class="play-button"><i class="fas fa-play"></i></button>
+                </div>
+            </div>
+            <div class="post-info">
+                <div class="post-author-infos">
+                    <img class="profile-picture" src="/public/img/profiles/alergyonthestage.png" alt="Profile picture">
+                    <div class="author-infos">
+                        <div class="author-name">
+                            <p>alergyonthestage</p>
+                        </div>
+                        <div class="time-info">
+                            <p>5 ore fa</p>
+                        </div>
+                    </div> 
+                </div>
+                <div class="post-song-infos">
+                    <div class="post-song-title">
+                        <p>Into You - Ariana</p>
+                    </div>
+                    </div>
+                    <div class="post-interactive-buttons">
+                    <button class="post-button active">
+                        <i class="fas fa-heart"></i>
+                    </button>
+                    <button class="post-button">
+                        <i class="fas fa-comment"></i>
+                    </button>
+                    <button class="post-button">
+                        <i class="fas fa-user"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="post-description">
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
+        </div>
+        <hr class="post-divider">
+    </div>
+    <div class="post">
+        <div class="post-header">
+            <div class="covert-art">
+                <div class="cover-square">
+                    <img class="cover-disc" src="/public/img/cover/1.jpg" alt="Cover-Art"/>
+                    <button class="play-button"><i class="fas fa-play"></i></button>
+                </div>
+            </div>
+            <div class="post-info">
+                <div class="post-author-infos">
+                    <img class="profile-picture" src="/public/img/profiles/alergyonthestage.png" alt="Profile picture">
+                    <div class="author-infos">
+                        <div class="author-name">
+                            <p>alergyonthestage</p>
+                        </div>
+                        <div class="time-info">
+                            <p>5 ore fa</p>
+                        </div>
+                    </div> 
+                </div>
+                <div class="post-song-infos">
+                    <div class="post-song-title">
+                        <p>Into You - Ariana</p>
+                    </div>
+                    </div>
+                    <div class="post-interactive-buttons">
+                    <button class="post-button active">
+                        <i class="fas fa-heart"></i>
+                    </button>
+                    <button class="post-button">
+                        <i class="fas fa-comment"></i>
+                    </button>
+                    <button class="post-button">
+                        <i class="fas fa-user"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="post-description">
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
+        </div>
+        <hr class="post-divider">
+    </div>
+   
 </div>
