61d487fca80d38afb20ade71c1fe5dc5b198e5e4
Emanuele Bertolero <bertoleroemanuele@gmail.com>
2023-06-23 12:10:04 +0200

    Created a first draft of a post

--- src/Website/views/feed.php
+++ src/Website/views/feed.php
@@ -4,6 +4,42 @@
 </div>
 <div class="post-container">
     <div class="post">
-
-    </div>  
+        <div class="covert-art">
+            <div class="cover-square">
+                <img class="cover-disc" src="/public/img/cover/1.jpg" alt="Cover-Art"/>
+                <button class="play-button"><i class="fas fa-play"></i></button>
+            </div>
+        </div>
+        <div class="post-info">
+            <div class="post-author-infos">
+                <img class="profile-picture" src="/public/img/profiles/alergyonthestage.png" alt="Profile picture">
+                <div class="author-infos">
+                    <div class="author-name">
+                        <p>alergyonthestage</p>
+                    </div>
+                    <div class="time-info">
+                        <p>5 ore fa</p>
+                    </div>
+                </div>
+                 
+            </div>
+            <div class="post-song-infos">
+                <div class="post-song-title">
+                    <p>Into You - Ariana</p>
+                </div>
+            </div>
+            <div class="post-interactive-buttons">
+            <button class="post-button active">
+                <i class="fas fa-heart"></i>
+            </button>
+            <button class="post-button">
+                <i class="fas fa-comment"></i>
+            </button>
+            <button class="post-button">
+                <i class="fas fa-user"></i>
+            </button>
+            </div>
+        </div>
+    </div>
+    
 </div>
