29d332e66477533bfd1d1b9626d3731392796448
Emanuele Bertolero <bertoleroemanuele@gmail.com>
2023-06-26 16:44:27 +0200

    Added suffix "feed-" in css

--- src/Website/views/feed.php
+++ src/Website/views/feed.php
@@ -1,59 +1,59 @@
 <view-head>
     <link rel="stylesheet" href="/public/css/feedStyle.css">
 </view-head>
-<div class="scroll-navigation">
-   <button class="scroll-nav-button active">Feed</button>
-   <button class="scroll-nav-button">Explore</button>
+<div class="feed-scroll-navigation">
+   <button class="feed-scroll-nav-button feed-active">Feed</button>
+   <button class="feed-scroll-nav-button">Explore</button>
 </div>
-<div class="post-container">
+<div class="feed-post-container">
 <?php foreach ($data as $post): extract($post) ?>
-<div class="post">
-        <div class="post-header">
-            <div class="covert-art">
-                <div class="cover-square">
-                    <img class="cover-disc" src="<?=$cover_art?>" alt="Cover-Art"/>
-                    <button class="play-button"><i class="fas fa-play"></i></button>
+<div class="feed-post">
+        <div class="feed-post-header">
+            <div class="feed-covert-art">
+                <div class="feed-cover-square">
+                    <img class="feed-cover-disc" src="<?=$cover_art?>" alt="Cover-Art"/>
+                    <button class="feed-play-button"><i class="fas fa-play"></i></button>
                 </div>
             </div>
-            <div class="post-infos">
+            <div class="feed-post-infos">
                 <a href="/user/<?=$username?>">
-                <div class="post-author-infos">
-                    <img class="profile-picture" src="<?=$profile_picture?>" alt="Profile picture">
-                    <div class="author-infos">
-                        <div class="author-name">
+                <div class="feed-post-author-infos">
+                    <img class="feed-profile-picture" src="<?=$profile_picture?>" alt="Profile picture">
+                    <div class="feed-author-infos">
+                        <div class="feed-author-name">
                             <p><?=$username?></p>
                         </div>
-                        <div class="time-info">
+                        <div class="feed-time-info">
                             <p><?=$time_ago?></p>
                         </div>
                     </div> 
                 </div>
                 </a>
-                <div class="post-song-infos">
-                    <div class="post-song-title">
+                <div class="feed-post-song-infos">
+                    <div class="feed-post-song-title">
                         <p><?=$song_title?></p>
                     </div>
-                    <div class="song-progress-bar">
-                        <div class="song-progress-bar-inner"></div>
+                    <div class="feed-song-progress-bar">
+                        <div class="feed-song-progress-bar-inner"></div>
                     </div>
                     </div>
-                    <div class="post-interactive-buttons">
-                    <button class="post-button active">
+                    <div class="feed-post-interactive-buttons">
+                    <button class="feed-post-button feed-active">
                         <i class="fas fa-heart"></i>
                     </button>
-                    <button class="post-button">
+                    <button class="feed-post-button">
                         <i class="fas fa-comment"></i>
                     </button>
-                    <button class="post-button">
+                    <button class="feed-post-button">
                         <i class="fas fa-user"></i>
                     </button>
                 </div>
             </div>
         </div>
-        <div class="post-description">
+        <div class="feed-post-description">
             <p><?=$description?></p>
         </div>
-        <hr class="post-divider">
+        <hr class="feed-post-divider">
     </div>
 <?php endforeach; ?>
    
