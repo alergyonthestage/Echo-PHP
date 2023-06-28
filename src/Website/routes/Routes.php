1a27e93315d7e290df4f8a855d8569b97dbe7dda
Alessandro <alessandro.antonini6@studio.unibo.it>
2023-06-28 16:51:02 +0200

    aaa pt2

--- src/Website/routes/Routes.php
+++ src/Website/routes/Routes.php
@@ -7,15 +7,10 @@ use CaveResistance\Echo\Website\App\Http\Controllers\FeedController;
 
 Server::createRoute()->accept('GET', '/home')->setHandler(HomeController::class)->add();
 
-Server::createRoute()->accept('GET', '/user/{username}')->setHandler(UserController::class)->add();
-
 Server::createRoute()->accept('GET', ['/', '/feed'])->setHandler(FeedController::class)->add();
 
-//Register
-Server::createRoute()->accept('GET', '/register')->setHandler([
-    'controller' => UserController::class,
-    'method' => 'register'
-])->add();
+//----Users----
+Server::createRoute()->accept('GET', '/user/{username}')->setHandler(UserController::class)->add();
 
 Server::createRoute()->accept(['GET', 'POST'], '/singup')->setHandler([
     'controller' => UserController::class,
