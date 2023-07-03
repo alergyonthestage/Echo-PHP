e9772224bce0880851fc727ebc1458b27048c2e4
Emanuele Bertolero <bertoleroemanuele@gmail.com>
2023-07-03 16:16:06 +0200

    Added html to the post in JS

--- src/Website/public/javascript/components/Feed.js
+++ src/Website/public/javascript/components/Feed.js
@@ -5,12 +5,35 @@ class Feed extends HTMLElement {
     constructor() {
         super();
         this.render(10);
+        this.isScrolling = false;
+        this.detectScrollToBottom();
     }
 
     render($initialPostsNumber) {
+        this.innerHTML += '<div class="feed-post-container">';
         for(let $i = 0; $i < $initialPostsNumber; $i++) {
-            this.innerHTML += new Post({title: 'Post '+$i}).render();
+            this.innerHTML += new Post({cover_art: 'public/img/cover/1.png', description: 'Ciaociao'}).render();
         }
+        this.innerHTML += '</div>';
+    }
+
+    detectScrollToBottom() {
+        window.addEventListener('scroll', () => {
+        const scrollPosition = window.pageYOffset;
+        const windowSize = window.innerHeight;
+        const bodyHeight = document.body.offsetHeight;
+        const feedPostContainer = this.querySelector('.feed-post-container');
+        const lastPost = feedPostContainer.lastElementChild;
+    
+        if (scrollPosition + windowSize >= bodyHeight && !this.isScrolling && lastPost) {
+          const lastPostOffset = lastPost.offsetTop + lastPost.offsetHeight;
+          if (lastPostOffset <= scrollPosition + windowSize) {
+            this.isScrolling = true;
+            this.render(1);
+            this.isScrolling = false;
+          }
+        }
+        });
     }
 
 }
--- src/Website/public/javascript/components/Post.js
+++ src/Website/public/javascript/components/Post.js
@@ -6,8 +6,55 @@ export default class Post {
 
     render() {
         return `
-        <div>
-            <h2>${this.data.title}</h2>
+        <div class="feed-post">
+            <div class="feed-post-header">
+                <div class="feed-covert-art">
+                    <div class="feed-cover-square">
+                        <img class="feed-cover-disc" src="${this.data.cover_art}" alt="Cover-Art"/>
+                        <button class="feed-play-button"><i class="fas fa-play"></i></button>
+                    </div>
+                </div>
+                <div class="feed-post-infos">
+                    <a href="/user/${this.data.username}">
+                    <div class="feed-post-author-infos">
+                        <div class="profile-picture-frame feed-profile-picture-frame">
+                            <img class="profile-picture" src="${this.data.profile_picture}" alt="Profile picture">
+                        </div>
+                        <div class="feed-author-infos">
+                            <div class="feed-author-name">
+                                <p>${this.data.username}</p>
+                            </div>
+                            <div class="feed-time-info">
+                                <p>${this.data.time_ago}</p>
+                            </div>
+                        </div> 
+                    </div>
+                    </a>
+                    <div class="feed-post-song-infos">
+                        <div class="feed-post-song-title">
+                            <p>${this.data.song_title}</p>
+                        </div>
+                        <div class="feed-song-progress-bar">
+                            <div class="feed-song-progress-bar-inner"></div>
+                        </div>
+                        </div>
+                        <div class="feed-post-interactive-buttons">
+                        <button class="feed-post-button feed-active">
+                            <i class="fas fa-heart"></i>
+                        </button>
+                        <button class="feed-post-button">
+                            <i class="fas fa-comment"></i>
+                        </button>
+                        <button class="feed-post-button">
+                            <i class="fas fa-user"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="feed-post-description">
+                <p>${this.data.description}</p>
+            </div>
+            <hr class="feed-post-divider">
         </div>
         `
     }
