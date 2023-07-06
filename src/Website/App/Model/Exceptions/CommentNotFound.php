8ac6e652c77275f59e523d97737e641854ea1edb
Angela Speranza <angela.speranza@studio.unibo.it>
2023-07-06 19:45:50 +0200

    Update CommentNotFound.php

--- src/Website/App/Model/Exceptions/CommentNotFound.php
+++ src/Website/App/Model/Exceptions/CommentNotFound.php
@@ -7,16 +7,35 @@ use Throwable;
 
 class CommentNotFound extends Exception {
 
-    private int $commentID;
+    private int $postID;
+    private int $userID;
+    private string $date;
+    private string $time;
 
-    public function __construct(int $commentID, $code = 0, Throwable $previous = null) 
+    public function __construct(int $postID, int $userID, string $date, string $time, $code = 0, Throwable $previous = null) 
     {
-        $this->commentID = $commentID;
-        parent::__construct("The comment ".$this->commentID." was not found on Echo servers.", $code, $previous);
+        $this->postID = $postID;
+        $this->userID = $userID;
+        $this->date = $date;
+        $this->time = $time;
+        parent::__construct("The comment was not found on Echo servers.", $code, $previous);
+    }
+
+    public function getPostID() {
+        return $this->postID;
+    }
+
+    public function getUserID() {
+        return $this->userID;
+    }
+
+    public function getDate() {
+        return $this->date;
+    }
+
+    public function getTime() {
+        return $this->time;
     }
     
-    public function getCommentID() {
-        return $this->commentID;
-    }
 
 }
