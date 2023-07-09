9c30ef57661b4de473b4b5ace2521caa5104b5ca
alergyonthestage <alessandro.antonini6@studio.unibo.it>
2023-07-09 17:15:06 +0200

    Update Feed.php

--- src/Website/App/Model/Feed.php
+++ src/Website/App/Model/Feed.php
@@ -12,12 +12,24 @@ class Feed {
         $this->posts = $posts;
     }
 
-    public static function getPosts(int $id_user, int $from, int $to) {
-        return new static(static::fetch($id_user, $from, $to));
+    public static function getPosts(int $id_user, int $offset, int $quantity) {
+        return new static(static::fetch($id_user, $offset, $quantity));
     }
 
-    private static function fetch(int $id_user, int $from, int $to) {
+    private static function fetch(int $id_user, int $offset, int $quantity) {
         $connection = Database::connect();
+
+        //tutti i post il cui autore è amico com l'utente loggato, ordinati per data e ora
+        $stmt = $connection->prepare("SELECT * FROM post WHERE id_user");
+        $stmt->bind_param('i', $id_post);
+        if(!$stmt->execute()){
+            throw new Exception("Database error");
+        }
+        $result = $stmt->get_result();
+        if(mysqli_num_rows($result) === 0) {
+            throw new PostNotFound($id_post);
+        }
+        $post = $result->fetch_array();
     }
 
 }
