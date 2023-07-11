4c37f0ca494c86300f32525e6b051b7617fad549
Emanuele Bertolero <bertoleroemanuele@gmail.com>
2023-07-11 16:23:49 +0200

    Added cover of new song

--- src/Website/views/user/user.php
+++ src/Website/views/user/user.php
@@ -2,6 +2,7 @@
     <link rel="stylesheet" href="/public/css/views/user.css">
     <link rel="stylesheet" href="/public/css/components/avatar.css">
     <link rel="stylesheet" href="/public/css/components/buttons.css">
+    <link rel="stylesheet" href="/public/css/components/badge.css">
     <link rel="stylesheet" href="/public/css/utils.css">
 </view-head>
 <div class="user-profile-header">
