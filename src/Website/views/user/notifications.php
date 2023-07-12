b6d190f7be77e7cae720a507e5d204d20f691fe2
Angela Speranza <angela.speranza@studio.unibo.it>
2023-07-12 16:36:49 +0200

    add rotellina dischetto carino

---
+++ src/Website/views/user/notifications.php
@@ -0,0 +1,8 @@
+<view-head>
+    <link rel="stylesheet" href="/public/css/views/user.css">
+    <link rel="stylesheet" href="/public/css/components/avatar.css">
+    <link rel="stylesheet" href="/public/css/components/buttons.css">
+    <link rel="stylesheet" href="/public/css/components/badge.css">
+    <link rel="stylesheet" href="/public/css/utils.css">
+</view-head>
+<div id="notifications"></div>
