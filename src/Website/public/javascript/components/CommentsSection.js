624d74b970c9a1fa158dfa9e4faddbb83359ea28
Emanuele Bertolero <bertoleroemanuele@gmail.com>
2023-07-17 16:48:07 +0200

    fixed not showing comment input text

--- src/Website/public/javascript/components/CommentsSection.js
+++ src/Website/public/javascript/components/CommentsSection.js
@@ -7,24 +7,23 @@ export default class CommentsSection {
     }
 
     render() {
+        const header = `<div class="comment-section-header">
+                            <button class="hide-comment-section-button" id=hide-comment-section-button><i class="fa-solid fa-angle-down"></i></button>
+                        </div>`
+        const footer = `<div class="comment-publish-area">
+                            <input maxlength="255" type="textarea" comment-publish-text="${this.data.postID}" placeholder="Write a comment..."></input>
+                            <button class="post-comment-publish-button" comment-publish-button="${this.data.postID}"><i class="fas fa-paper-plane"></i></button>
+                        </div>`
         if(this.data.length === 0) {
-            return '<button class="hide-comment-section-button" id=hide-comment-section-button><i class="fa-solid fa-angle-down"></i></button></br>No comments yet'
+            return header+'No comments yet'+footer
         }
         let commentsHTML = '';
         this.data.forEach(comment => {
             commentsHTML += new Comment(comment).render()
         });
-        return `<div class="comment-section-header">
-                    <button class="hide-comment-section-button" id=hide-comment-section-button><i class="fa-solid fa-angle-down"></i></button>
-                </div>   
-                <div class="comments-section-list">
+        return header+`<div class="comments-section-list">
                     ${commentsHTML}
-                </div>
-                <div class="comment-publish-area">
-                            <input maxlength="255" type="textarea" comment-publish-text="${this.data.postID}" placeholder="Write a comment..."></input>
-                            <button class="post-comment-publish-button" comment-publish-button="${this.data.postID}"><i class="fas fa-paper-plane"></i></button>
-                    </div>
-                `
+                </div>`+footer
     }
 
 }
