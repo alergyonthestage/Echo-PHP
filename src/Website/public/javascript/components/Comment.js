b02d0b02869f2c882736d6f4cce91d051d89e0f8
Angela Speranza <angela.speranza@studio.unibo.it>
2023-07-18 10:53:22 +0200

    accessibilità

--- src/Website/public/javascript/components/Comment.js
+++ src/Website/public/javascript/components/Comment.js
@@ -2,7 +2,7 @@ export default class Comment {
 
     constructor(data) {
         this.data = data
-        this.badge = this.data.author.isVerified ? `<i class="fas fa-check-circle"></i>` : "";
+        this.badge = this.data.author.isVerified ? `<em class="fas fa-check-circle"></em>` : "";
     }
 
     render() {
@@ -13,7 +13,7 @@ export default class Comment {
                     <div class="post-comment-content">
                         <div class="post-comment-text">
                             <p>
-                                <a href="/user/${this.data.author.username}"><b>@${this.data.author.username} ${this.badge}</b></a> 
+                                <a href="/user/${this.data.author.username}"><strong>@${this.data.author.username} ${this.badge}</strong></a> 
                                 ${this.data.text}
                             </p>
                         </div>
@@ -32,7 +32,7 @@ export default class Comment {
                     <div class="post-comment-content-compact">
                         <div class="post-comment-text">
                             <p>
-                                <a href="/user/${this.data.author.username}"><b>@${this.data.author.username} ${this.badge}</b></a> 
+                                <a href="/user/${this.data.author.username}"><strong>@${this.data.author.username} ${this.badge}</strong></a> 
                                 <br>${this.data.text}
                             </p>
                         </div>
--- src/Website/public/javascript/components/Post.js
+++ src/Website/public/javascript/components/Post.js
@@ -4,8 +4,12 @@ export default class Post {
 
     constructor(data) {
         this.data = data;
+<<<<<<< Updated upstream
         this.badge = this.data.author.isVerified ? `<i class="fas fa-check-circle"></i>` : "";
         console.log(data)
+=======
+        this.badge = this.data.author.isVerified ? `<em class="fas fa-check-circle"></em>` : "";
+>>>>>>> Stashed changes
     }
 
     render() {
@@ -13,7 +17,11 @@ export default class Post {
                     <div class="post-covert-art">
                         <div class="post-cover-square">
                             <img class="post-cover-disc" src="${this.data.song.coverArt}" alt="Cover-Art"/>
+<<<<<<< Updated upstream
                             <button class="post-play-button" play-button="${this.data.song.youtubeID}"><i class="fas fa-play"></i></button>
+=======
+                            <button class="post-play-button"><em class="fas fa-play"></em></button>
+>>>>>>> Stashed changes
                         </div>
                     </div>
                     <div class="post-infos">
@@ -42,11 +50,11 @@ export default class Post {
                             <div class="post-interaction-buttons">
                                 <span like-counter="${this.data.id}">${this.data.likesCount}</span>
                                 <button class="post-interaction-button ${this.data.liked ? "post-interaction-button-active": ""}" like-button="${this.data.id}">
-                                    <i class="fas fa-heart"></i>
+                                    <em class="fas fa-heart"></em>
                                 </button>
                                 <span comment-counter="${this.data.id}">${this.data.commentsCount}</span>
                                 <button class="post-interaction-button" comment-button="${this.data.id}">
-                                    <i class="fas fa-comment"></i>
+                                    <em class="fas fa-comment"></em>
                                 </button>
                             </div>
                         </div>
