669a47d80401014ec35230972093abcc34002e78
Emanuele Bertolero <bertoleroemanuele@gmail.com>
2023-07-18 17:59:39 +0200

    added play/button switch

--- src/Website/public/javascript/post/interactions/reproduction.js
+++ src/Website/public/javascript/post/interactions/reproduction.js
@@ -1,6 +1,7 @@
 let currentVideoID = null
 
 export function play(videoID) {
+    const playButton = document.querySelector(`[play-button="${videoID}"]`);
     if(window.ytPlayer !== null) {
         if (currentVideoID !== videoID) {
             window.ytPlayer.loadVideoById(videoID, 0, 'small')
@@ -8,8 +9,11 @@ export function play(videoID) {
         }
         if(window.ytPlayer.getPlayerState() === YT.PlayerState.PLAYING) {
             window.ytPlayer.pauseVideo()
+            playButton.innerHTML = '<em class="fas fa-play"></em>'
+
         } else {
             window.ytPlayer.playVideo()
+            playButton.innerHTML = '<em class="fas fa-pause"></em>'
         }
     } else {
         console.log('Error: player not found')
